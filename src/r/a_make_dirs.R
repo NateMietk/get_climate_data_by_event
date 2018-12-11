1b7ab2dfe06e5143714b4cdffcbb1b75bd53867a
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-12-11 09:14:55 -0700

    removed climate extract dir

--- src/r/a_make_dirs.R
+++ src/r/a_make_dirs.R
@@ -35,9 +35,6 @@ raw_dir_mtbs <- file.path(raw_dir, "mtbs")
 proc_dir_mtbs <- file.path(processed_dir, 'mtbs')
 proc_dir_modis <- file.path(processed_dir, 'modis')
 
-# create direcotires to hold climate extraction outputs
-extraction_climate_dir <- file.path(extraction_dir, "climate_extractions")
-
 # for pushing and pulling to s3 using the system function
 s3_base <- 's3://earthlab-natem/get_climate_data_megafires/'
 s3_proc_prefix <- 's3://earthlab-natem/get_climate_data_megafires/processed/'
@@ -46,6 +43,6 @@ s3_proc_climate <- 's3://earthlab-natem/get_climate_data_megafires/fire/'
 
 # Check if directory exists for all variable aggregate outputs, if not then create
 var_dir <- list(data_dir, raw_dir, extraction_dir, processed_dir, raw_dir_us, raw_dir_mtbs,
-                proc_dir_mtbs, proc_dir_modis, extraction_climate_dir)
+                proc_dir_mtbs, proc_dir_modis)
 
 lapply(var_dir, function(x) if(!dir.exists(x)) dir.create(x, showWarnings = FALSE))
