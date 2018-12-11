2296cfd42cd4430945c9914fbb2ac8eca2beb390
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-12-11 09:14:46 -0700

    finialized - saved as csv

--- src/r/d_extract_climate_vars.R
+++ src/r/d_extract_climate_vars.R
@@ -1,4 +1,4 @@
-if(!file.exists(file.path(extraction_dir, 'climate_megafires.rds'))) {
+if(!file.exists(file.path(extraction_dir, 'climate_megafires.csv'))) {
   climate_vars <- c('pr', 'rmin', 'rmax', 'tmmx', 'tmmn', 'vpd', 'th', 'vs', 'pet', 'fm100', 'fm1000')
   climate_extracted <- NULL
   climate_per_mtbs_id <- NULL
@@ -71,10 +71,10 @@ if(!file.exists(file.path(extraction_dir, 'climate_megafires.rds'))) {
   climate_megafires <- bind_rows(climate_per_mtbs_id) %>%
     dplyr::select(-fire_id1, -fire_id2, -fire_id3, -fire_id4, -fire_id5, -fire_id6, -fire_id7, 
                   -fire_id8, -fire_id9, -fire_id10) %>%
-    select(fire_id, everything())
-  write_rds(climate_megafires, file.path(extraction_dir, 'climate_megafires.rds'))
+    left_join(as.data.frame(fire_event_extract) %>% dplyr::select(-geom), ., by = 'fire_id')
+  write_csv(climate_megafires, file.path(extraction_dir, 'climate_megafires.csv'))
   system(paste0('aws s3 sync ', extraction_dir, ' ', s3_proc_extractions))
   
 } else {
-  climate_megafires <- st_read(file.path(extraction_dir, 'climate_megafires.rds'))
+  climate_megafires <- read_csv(file.path(extraction_dir, 'climate_megafires.csv'))
 }
